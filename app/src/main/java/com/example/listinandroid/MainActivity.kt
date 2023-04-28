15ce2686d716e4a9919e539edf4fb61696c54816
tarek <mohiuddintarek@gmail.com>
2023-04-28 19:57:27 +0600

    list view

--- app/src/main/java/com/example/listinandroid/MainActivity.kt
+++ app/src/main/java/com/example/listinandroid/MainActivity.kt
@@ -1,35 +1,30 @@
 package com.example.listinandroid
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var listView: ListView
+    var myNumber = mutableListOf("Hello", "Mello","Hello 3 ", "Hello 5")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val title = supportActionBar
+        title!!.title = "List View "
         listView = findViewById(R.id.homeListView)
-
-        initListView()
-    }
-
-    private fun initListView() {
-        val arrayAdapter =
-            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, DummyFile().teamPlayer)
-
-        listView.adapter = arrayAdapter
-
+        val numberAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, myNumber)
+        listView.adapter = numberAdapter
 
         listView.setOnItemClickListener { adapterView, view, i, l ->
-            DummyFile().teamPlayer.removeAt(i)
-            arrayAdapter.notifyDataSetChanged()
-            Toast.makeText(this,"You clicked on ${i}",Toast.LENGTH_SHORT).show()
+            //myNumber.add(0,"New Value{$myNumber.size+1} ")
+            //myNumber.removeAt(i)
+            //myNumber.reversed()
+            //myNumber.shuffle()
+            numberAdapter.notifyDataSetChanged()
         }
-    }
-    private fun removeOneItemFromList(index:Int){
-        DummyFile().teamPlayer.reversed()
+
     }
 }
